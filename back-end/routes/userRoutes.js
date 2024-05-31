a6d3d557cb70145add711c814f54f585c8dab43b
shruthi1993s <shruthiji06@gmail.com>
2024-05-31 13:03:37 +0530

    Nodejs

---
+++ back-end/routes/userRoutes.js
@@ -0,0 +1,68 @@
+const express=require("express")
+const User=require("../models/User")
+
+const router=express.Router();
+
+router.post("/users",async(req,res)=>{
+    try{
+        const user=new User(req.body)
+        await user.save();
+        res.status(201).send(user)
+    }
+    catch(error){
+        res.status(400).send(error)
+    }
+})
+
+router.get("/users",async(req,res)=>{
+    try{
+        const user=await User.find({})
+       
+        res.status(201).send(user)
+    }
+    catch(error){
+        res.status(400).send(error)
+    }
+})
+
+router.get("/users/:id",async(req,res)=>{
+    try{
+        const user=await User.findById(req.params.id)
+       if(!user){
+        res.status(404).send()
+       }
+       res.status(201).send(user) 
+    }
+        catch(error){
+        res.status(400).send(error)
+    }
+})
+
+
+router.put("/users/:id",async(req,res)=>{
+    try{
+        const user=await User.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true})
+       if(!user){
+        res.status(404).send()
+       }
+       res.status(201).send(user) 
+    }
+        catch(error){
+        res.status(400).send(error)
+    }
+})
+
+router.delete("/users/:id",async(req,res)=>{
+    try{
+        const user=await User.findByIdAndDelete(req.params.id)
+       if(!user){
+        res.status(404).send()
+       }
+       res.status(201).send(user) 
+    }
+        catch(error){
+        res.status(400).send(error)
+    }
+})
+
+module.exports=router;
---
+++ back-end/server.js
@@ -0,0 +1,37 @@
+
+
+const express=require("express")
+const mongoose=require("mongoose")
+const bodyParser=require("body-parser")
+const userRoutes=require("./routes/userRoutes")
+
+require(dotenv).config()
+
+const cors=require("cors")
+const app=express()
+
+const PORT=3000;
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use("/apiUser",userRoutes)
+mongoose.connect("process.env.MongoDB")
+.then(()=>{
+    console.log("Connected to MONGODB")
+    app.listen(process.env.PORT,()=>{
+    
+        console.log(`Server is running on port ${PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("Connection error",error.message)
+})
+
+function displayName(firstname,secondname){
+    return `Hey,${firstname}${secondname} !!!`
+}
+
+app.get('/',(req,res)=>{
+    res.json(displayName("Hasma","Shruthi"))
+})
+
